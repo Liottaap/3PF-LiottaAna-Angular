089746ccb8554ac1d4562ff06a7bbb1ca9d323d2
Ana Liotta <liotta.a.p@gmail.com>
2025-05-26 17:34:57 -0300

    Agregado test de confirmacion de alumno

--- src/app/modules/dashboard/modules/alumnos/alumnos.component.spec.ts
+++ src/app/modules/dashboard/modules/alumnos/alumnos.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlumnosComponent } from './alumnos.component';
 import { HttpClientTestingModule, provideHttpClientTesting } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { AlumnosService } from './alumnos.service';
 
 describe('AlumnosComponent', () => {
   let component: AlumnosComponent;
@@ -23,4 +25,25 @@ describe('AlumnosComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+
+  it('Debe borrar alumno si se confirma', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    AlumnosService.deleteAlumno.and.returnValue(of({}));
+  
+    component.onDeleteAlumn(2);
+  
+    expect(AlumnosService.deleteAlumno).toHaveBeenCalledWith(2);
+  });
+  
+  it('No debe borrar alumno si no se confirma', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+  
+    component.onDeleteAlumn(2);
+  
+    expect(AlumnosService.deleteAlumno).not.toHaveBeenCalled();
+  });
 });
+
+
